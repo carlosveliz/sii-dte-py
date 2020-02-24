0c0f49099c7ea4849c344421c6f8583bba9a6949
SunPaz <matth.amoros@gmail.com>
2020-02-24 13:51:27 -0800

    Added note and requirements

--- lib/certificate-service.py
+++ lib/certificate-service.py
@@ -5,3 +5,28 @@
  2- Sign
 
 """
+
+from lxml import etree
+from signxml import XMLSigner, XMLVerifier
+
+class CertificateService:
+	def __init__(self):
+		print("Not implemented.")
+
+	def set_certificate(self, path):
+		""" How to safely store certificate ? """
+		print("Not implemented.")
+
+	def sign(self, paylaod, use_loaded_certificate=True, certificate=None):
+		""" From signXML sample code """
+		""" PFX file usually contain certificate and private key """
+		""" Extract from PFX file """
+		""" openssl pkcs12 -in [yourfile.pfx] -nocerts -out [keyfile-encrypted.key] """
+		""" openssl pkcs12 -in [yourfile.pfx] -clcerts -nokeys -out [certificate.crt] """
+		""" openssl rsa -in [keyfile-encrypted.key] -out [keyfile-decrypted.key] """
+		cert = open("example.pem").read()
+		key = open("example.key").read()
+		root = etree.fromstring(paylaod)
+		signed_root = XMLSigner().sign(root, key=key, cert=cert)
+
+		return signed_root
