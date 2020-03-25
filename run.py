3a5f1c48d1a31e44e6f669a0fe29cfdaff704910
SunPaz <matth.amoros@gmail.com>
2020-03-25 06:10:39 -0700

    Added "help" command and others

--- run.py
+++ run.py
@@ -4,14 +4,17 @@ It gets a copy of the app from your package and runs it.
 """
 
 __version__ = '0.1'
-
-from lib.web import app
-from lib.models.sii_token import Token
-from lib.sii_connector_auth import SiiConnectorAuth
-from lib.certificate_service import CertificateService
-from instance.config import FLASK_LISTEN_PORT, FLASK_ENDPOINT, DEBUG_MODE
 import logging
 import sys
+import json
+from lib.web import app
+from lib.models.sii_token import Token
+from lib.models.dte import DTECAF, DTEBuidler
+from lib.sii_connector_auth import SiiConnectorAuth
+from lib.certificate_service import CertificateService
+from lib.pdf_generator import PDFGenerator
+from instance.config import FLASK_LISTEN_PORT, FLASK_ENDPOINT, DEBUG_MODE
+
 
 if DEBUG_MODE == True:
 	logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s | %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
@@ -21,9 +24,12 @@ else:
 logging.info('Application started.')
 
 if len(sys.argv) > 1:
-	logging.warning('Diagnose started.')
-	diagnose_type = sys.argv[1]
-	if diagnose_type == "0":
+	command = sys.argv[1]
+	if command == "help":
+		print("get_token <pfx_file_path> <pfx_password>")
+		print("generate_cert <pfx_file_path> <pfx_password>")
+		print("generate_pdf <sii type>")
+	if command == "get_token":
 		logging.warning('Full test.')
 		auth = SiiConnectorAuth(pfx_file_path=sys.argv[2], pfx_password=sys.argv[3])
 		seed = auth.get_seed()
@@ -34,23 +40,40 @@ if len(sys.argv) > 1:
 								pfx_password=sys.argv[3])
 		token = Token(auth.get_token(seed))
 		print("Token : " + token)
-	if diagnose_type == "1":
-		""" Get seed, build token, exit """
-		logging.warning('Authentication test.')
-		auth = SiiConnectorAuth()
-		seed = auth.get_seed()
-		print("Seed : " + seed)
-		auth = SiiConnectorAuth(server='maullin', \
-								module=SiiConnectorAuth.GET_TOKEN_MODULE_ID, \
-								pfx_file_path=sys.argv[2], \
-								pfx_password=sys.argv[3])
-		token = auth.get_token(seed)
-		print("Token : " + token)
-	elif diagnose_type == "2":
+	if command == "generate_cert":
 		""" Generate certificate only """
 		logging.warning('Certificate generation test.')
 		cert = CertificateService(pfx_file_path=sys.argv[2], pfx_password=sys.argv[3])
 		cert.generate_certificate_and_key()
+	if command =="generate_pdf":
+			pdf = PDFGenerator()
+			""" Dump test XML """
+			type = int(sys.argv[2])
+
+			sender_parameters = {}
+			receiver_parameters = {}
+			specific_header_parameters = {}
+			item_list = {}
+
+			with open('test/data/sender.json') as json_file:
+				sender_parameters = json.load(json_file)
+			with open('test/data/receiver.json') as json_file:
+				receiver_parameters = json.load(json_file)
+			with open('test/data/items.json') as json_file:
+				item_list = json.load(json_file)
+			with open('test/data/specifics.json') as json_file:
+				specific_header_parameters = json.load(json_file)
+
+			caf = DTECAF(parameters={}, signature='', private_key='')
+			caf.load_from_XML('test/data/caf_test.xml')
+
+			builder = DTEBuidler()
+
+			_, pretty_dte, dte_object = builder.build(type, sender_parameters, receiver_parameters, specific_header_parameters, item_list, caf)
+			pdf.generate(dte_object)
+
+			myXML = open('temp/DTE_' + str(type) + '.xml', "w")
+			myXML.write(pretty_dte)
 else:
 	"""
 	  Run Flask web app
