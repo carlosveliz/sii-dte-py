b9bd8b24da3fa2848d280153c906144392600dcd
SunPaz <matth.amoros@gmail.com>
2020-03-25 06:22:44 -0700

    Fixed a lot of stupid issues...

--- lib/models/sii_token.py
+++ lib/models/sii_token.py
@@ -5,21 +5,21 @@ class Token():
 	_expiry_date = None
 
 	def __init__(self, token_string):
-		_token_string = token_string
+		self._token_string = token_string
 		""" Expire after 10 min """
-		_expiry_date = datetime.now().min + 10
+		self._expiry_date = datetime.datetime.now() + datetime.timedelta(0,60*10)
 
 	def __str__(self):
 		return _token_string
 
-	def get_token(sef):
-		if not _is_expired:
-			return _token_string
+	def get_token(self):
+		if not self._is_expired():
+			return self._token_string
 		else:
 			raise TokenExpiredError()
 
 	def _is_expired(self):
-		return datetime.now() >= _expiry_date
+		return datetime.datetime.now() >= self._expiry_date
 
 class TokenExpiredError(Exception):
 	""" Raised on token expiration """
