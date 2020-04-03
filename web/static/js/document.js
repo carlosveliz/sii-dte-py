63f570389ac8bec5fd01592b7fc1c2fa794b14ca
SunPaz <matth.amoros@gmail.com>
2020-04-03 11:52:51 -0700

    Unflatten inputs name to retrieve original parameters structure

--- web/static/js/document.js
+++ web/static/js/document.js
@@ -1,6 +1,57 @@
 function saveDocument(documentDivId) {
 	let parameters = $('#' + documentDivId).find('input, select, textarea').serializeArray();
-	console.log(parameters);
+	let correctFormat = buildCompatibleJsonModel(parameters);
+	$.ajax({
+		type: 'POST',
+		/**/
+		url: '/dte',
+		/**/
+		dataType: 'application/json',
+		data: JSON.stringify(correctFormat),
+		success: function () {
+
+		},
+		error: function (ex) {
+		}
+	});
+}
+
+function buildCompatibleJsonModel(parameters) {
+	let correctFormat = {};
+
+	for (param in parameters) {
+		let item = parameters[param];
+		if(item.name.indexOf('-') > 0)
+		{
+			setItemByPath(item.value, item.name, correctFormat)
+		}
+		else {
+			correctFormat[item.name] = item.value;
+		}
+	}
+
+	return correctFormat;
+}
+
+//Unflatten items
+function setItemByPath(value, path, array)
+{
+	let pathNode = path.split('-');
+	let node = path.split('-')[0];
+	if(pathNode.length == 1)
+	{
+		//End of path
+		array[node] = value;
+	}
+	else
+	{
+		if(!(node in array))
+		{
+			array[node] = {};
+			//Remove node and pass path
+			setItemByPath(value, path.replace(node + '-', ''), array[node]);
+		}
+	}
 }
 
 function addNewDetailRow() {
@@ -15,17 +66,17 @@ function addNewDetailRow() {
 
 	//Update inputs name with unique id
 	elements.each(function(element) {
-			let elem = $(elements[element]);
-			elem.attr('name', rowCount + '-' + elem.attr('name'));
-			if(!elem.hasClass("remove"))
-				elem.attr('disabled', true);
-			else {
-				elem.attr('disabled', false);
-			}
+		let elem = $(elements[element]);
+		elem.attr('name', 'Details' - rowCount + '-' + elem.attr('name').split('-')[1]);
+		if(!elem.hasClass("remove"))
+		elem.attr('disabled', true);
+		else {
+			elem.attr('disabled', false);
 		}
-	);
+	}
+);
 
-	target.after(newRow);
+target.after(newRow);
 }
 
 function removeNewDetailRow(target) {
