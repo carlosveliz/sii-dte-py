84fbb8f48ed56d961115e1128865590d9b94803a
SunPaz <matth.amoros@gmail.com>
2020-04-09 14:25:18 -0700

    Massive refactoring....

--- lib/zeep/sii_plugin.py
+++ lib/zeep/sii_plugin.py
@@ -44,33 +44,8 @@ class SiiPlugin(Plugin):
 	def sign_tagged_message(self, tagged_message):
 		""" Add signature template """
 		template_string = self.read_file('cert/sign_sii_xml.tmpl').decode('utf-8')
-		tagged_message = tagged_message.replace(SIGNATURE_TAG, template_string)
-
-		logger = logging.getLogger()
-		"""Should sign a file using a dynamicaly created template, key from PEM and an X509 cert."""
-		assert(tagged_message)
-		# Load the pre-constructed XML template.
-		template = etree.fromstring(tagged_message)
-
-		# Find the <Signature/> node.
-		signature_node = xmlsec.tree.find_node(template, xmlsec.Node.SIGNATURE)
-
-		assert signature_node is not None
-		assert signature_node.tag.endswith(xmlsec.Node.SIGNATURE)
-
-		ctx = xmlsec.SignatureContext()
-		try:
-			ctx.key = xmlsec.Key.from_memory(self.key, format=xmlsec.constants.KeyDataFormatPem)
-			ctx.key.load_cert_from_memory(self.cert, format=xmlsec.constants.KeyDataFormatCertPem)
-		except xmlsec.Error:
-			logger.error("SiiPlugin::sign Key or certificate could not be loaded.")
-			return ''
-
-		ctx.sign(signature_node)
-		template.remove(xmlsec.tree.find_node(template, xmlsec.Node.SIGNATURE))
-		template.insert(1, signature_node)
-		return etree.tostring(template, pretty_print=True).decode('utf-8')
-
+		tagged_message = tagged_message.replace(self.SIGNATURE_TAG, template_string)
+		return self.sign(tagged_message)
 
 	def sign(self, message_with_template_included):
 		logger = logging.getLogger()
